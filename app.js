1b31a7c4e25f6f1208c9bb381ce57e9131457b47
Jared Watson <watsonj@ITJW20.local>
2020-08-25 16:17:31 -0500

    adding functionality in JS

--- app.js
+++ app.js
@@ -1,2 +1,43 @@
-const textElement = document.getElementById('text');
-const optionButtons = document.getElementById('option-buttons');
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+    state = {}
+    showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+   const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+   textElement.innerText = textNode.text
+}
+
+function selectOption(option) {
+
+}
+
+const textNodes = [
+    {
+        id: 1,
+        text: "A body has been found at the library and the chief wants you to solve the case. Are you up for it, detective?",
+        options: [
+            {
+                text: "I'm up for it.",
+                setState: {
+                    takeCase: true
+                    },
+                nextText: 2
+            },
+            {
+                text: "I'll pass on this case",
+                nextText: 2
+            }
+        ]
+    },
+    {
+        id: 2
+    }
+]
+
+startGame();
